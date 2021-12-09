417f9d4e988673f6b725e19486b5e2bee2316deb
Haoyang Guo <haoyang.li.guo@gmail.com>
2021-12-08 19:10:32 -0800

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.secret_key = "kate"
 api = Api(app)
 
+
 @app.before_first_request
 def create_tables():
     db.create_all()
+
 
 jwt = JWT(app, authenticate, identify)
 
